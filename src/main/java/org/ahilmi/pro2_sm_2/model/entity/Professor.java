dd4566fb3caeb64789a51075ecdd195fa969bdcc
ahilmii <ahmet1905hilmi@gmail.com>
2026-04-10 22:03:00 +0300

    professor entity to dto transition applied

--- src/main/java/org/ahilmi/pro2_sm_2/model/entity/Professor.java
+++ src/main/java/org/ahilmi/pro2_sm_2/model/entity/Professor.java
@@ -5,6 +5,10 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.ahilmi.pro2_sm_2.dto.RequestCourseDTO;
+import org.ahilmi.pro2_sm_2.dto.RequestProfessorDTO;
+import org.ahilmi.pro2_sm_2.dto.ResponseCourseDTO;
+import org.ahilmi.pro2_sm_2.dto.ResponseProfessorDTO;
 
 import java.util.List;
 
@@ -28,4 +32,25 @@ public class Professor {
 
     @OneToMany(mappedBy = "professor", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Teaches> teaches;
+
+
+
+    // yeni kayıt oluştururken Request DTO'yu Entity'ye çeviren Constructor
+    public Professor(RequestProfessorDTO requestDTO) {
+        this.name = requestDTO.getName();
+        this.department = requestDTO.getDepartment();
+    }
+
+    public ResponseProfessorDTO viewAsProfessorDTO() { // entity'den response'a
+        ResponseProfessorDTO dto = new ResponseProfessorDTO();
+        dto.setId(this.id);
+        dto.setName(this.name);
+        dto.setDepartment(this.department);
+
+        if (this.teaches != null) {
+            dto.setTeaches(this.teaches.stream().map(Teaches::viewAsTeachesDTO).toList());
+        }
+
+        return dto;
+    }
 }
